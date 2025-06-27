1025be06ab1bbbf05d9e5ac2b00423174a6a56bf
Слава Учусь <Sykonec@mail.ru>
2025-06-27 20:19:45 +0300

    User confing add

--- src/main/java/com/naperstky/mvc/PlayerController.java
+++ src/main/java/com/naperstky/mvc/PlayerController.java
@@ -1,4 +1,28 @@
 package com.naperstky.mvc;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PlayerController {
+
+
+    @GetMapping
+    public String testMethod() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("программа работает");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        return "hello";
+    }
+
 }
